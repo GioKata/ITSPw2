7292861582b2d0f151fbb7de1a10c3a8b204a717
Giovanni <giovanni.rosanna04@gmail.com>
2024-03-23 18:13:15 +0100

    Fine pagine letteraDiRichiamo e Portale
    Sistemazione stile form letteradirichiamo

--- pw2Web/script/generaDaFile.js
+++ pw2Web/script/generaDaFile.js
@@ -40,17 +40,11 @@ function createCard(d){
             topDiv.classList.add("d-flex","flex-column","text-center","topDiv","pt-1");
             let dataDiv=document.createElement("div");
             dataDiv.classList.add("d-flex","flex-column","justify-content-center","dataDiv","p-3");
+            topDiv.classList.add(d.categoria);
+            topDiv.innerHTML='<h5>'+d.nome+" "+d.cognome+'<h5><h6>'+d.categoria+'<h6>';
             if(d.categoria==="dirigente"){
-                topDiv.classList.add("dirigente");
-                topDiv.innerHTML='<h5>'+d.nome+" "+d.cognome+'<h5><h6>Dirigente<h6>';
                 dataDiv.innerHTML='<p>Data assunzione: ' + d.dataAssunzione + '</p><p>Codice fiscale: ' + d.codiceFiscale + '</p>';
-            }else if(d.categoria==="tecnico"){
-                topDiv.classList.add("tecnico");
-                topDiv.innerHTML='<h5>'+d.nome+" "+d.cognome+'<h5><h6>Tecnico<h6>';
-                dataDiv.innerHTML='<p>Data assunzione: ' + d.dataAssunzione + '</p><p>Codice fiscale: ' + d.codiceFiscale + '</p><p>Referente: '+dR.nome+' '+dR.cognome+'</p>';
             }else{
-                topDiv.classList.add("manager");
-                topDiv.innerHTML='<h5>'+d.nome+" "+d.cognome+'<h5><h6>Manager<h6>';
                 dataDiv.innerHTML='<p>Data assunzione: ' + d.dataAssunzione + '</p><p>Codice fiscale: ' + d.codiceFiscale + '</p><p>Referente: '+dR.nome+' '+dR.cognome+'</p>';
             }
             card.appendChild(topDiv);
