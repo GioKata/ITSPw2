67e81356b25232ccb3aca66f40b14f9abdcb2740
Giovanni <giovanni.rosanna04@gmail.com>
2024-03-25 12:37:39 +0100

    Java Creazione menu iterativo per le 3 richieste

--- pw2Java/src/main/java/pw2/App.java
+++ pw2Java/src/main/java/pw2/App.java
@@ -8,28 +8,48 @@ import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args )
-    {      
-        String tip,r;
-        Azienda a=new Azienda("Erreci");
+    {   
+        String scelta,tip,r,r2;
         Scanner input=new Scanner(System.in);
+        System.out.println("Scegli il nome dell'azienda");
+        Azienda a=new Azienda(input.nextLine());
         createDaFile(a);
-        System.out.println("////////////////////STIPENDIO///////////////////");
-        a.stampaStipendio();
-        System.out.println("////////////////////ALFABETICO///////////////////");
-        a.sortAlfabetico();
-        System.out.println(a);
-        /*Ciclo tipologia dip ordine permanenza */
         do{
+            System.out.println("Menù");
             do{
-                System.out.println("inserisci la tipologia di Dipendente di cui vuoi saperer i dati anagrafici");
-                tip=input.nextLine();
-            }while(!tip.equalsIgnoreCase("Tecnico") && !tip.equalsIgnoreCase("Manager")  && !tip.equalsIgnoreCase("Dirigente"));
-            a.stampaScelta(tip);
+                System.out.println("1)Stipendio  2)Ordine Alfabetico  3)Tipologia");
+                scelta=input.nextLine();
+            }while(!scelta.equalsIgnoreCase("Stipendio") && !scelta.equalsIgnoreCase("1") && !scelta.equalsIgnoreCase("Ordine Alfabetico") && !scelta.equalsIgnoreCase("2") && !scelta.equalsIgnoreCase("Tipologia") && !scelta.equalsIgnoreCase("3"));
+            switch(scelta){
+                case "Stipendio": case "1":
+                    System.out.println("////////////////////STIPENDIO///////////////////");
+                    a.stampaStipendio();
+                    break;
+                case "Ordine Alfabetico": case "2":
+                    System.out.println("////////////////////ALFABETICO///////////////////");
+                    a.sortAlfabetico();
+                    System.out.println(a);
+                    break;
+                case  "Tipologia": case "3":
+                    do{
+                        do{
+                            System.out.println("inserisci la tipologia di Dipendente di cui vuoi sapere i dati anagrafici (Tecnico,Manager,Dirigente)");
+                            tip=input.nextLine();
+                        }while(!tip.equalsIgnoreCase("Tecnico") && !tip.equalsIgnoreCase("Manager")  && !tip.equalsIgnoreCase("Dirigente"));
+                        a.stampaScelta(tip);
+                        do{
+                            System.out.println("Voui continuare a scegliere  una tipologia di Dipendente y/n");
+                            r=input.nextLine();
+                        }while(!r.equalsIgnoreCase("y") && !r.equalsIgnoreCase("yes") && !r.equalsIgnoreCase("n") && !r.equalsIgnoreCase("no"));
+                    }while(r.equals("y") && !r.equalsIgnoreCase("yes"));
+                    break;
+            }
             do{
-                System.out.println("Voui continuare y/n");
-                r=input.nextLine();
-            }while(!r.equalsIgnoreCase("y") && !r.equalsIgnoreCase("yes") && !r.equalsIgnoreCase("n") && !r.equalsIgnoreCase("no"));
-        }while(r.equals("y") && !r.equalsIgnoreCase("yes"));
+                System.out.println("Sei tornato nel menù voui continuare y/n");
+                r2=input.nextLine();
+            }while(!r2.equalsIgnoreCase("y") && !r2.equalsIgnoreCase("yes") && !r2.equalsIgnoreCase("n") && !r2.equalsIgnoreCase("no")); 
+
+        }while(r2.equals("y") && !r2.equalsIgnoreCase("yes"));
         input.close();
     }
     public static void createDaFile(Azienda a){
