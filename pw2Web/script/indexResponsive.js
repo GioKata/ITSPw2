71626a83ac8bfa352581c1ba2cc69ec4048f20e4
Giovanni <giovanni.rosanna04@gmail.com>
2024-03-26 12:29:26 +0100

    Pagina home resa responsive
    tramite media query ultimi aggiustamenti ancora da fare

---
+++ pw2Web/script/indexResponsive.js
@@ -0,0 +1,14 @@
+document.getElementById("card-primo").addEventListener("click",function(){
+    document.getElementById("titoloCard").innerHTML="Personal Recruting";
+    document.getElementById("testoCard").innerHTML="Grazie alla nostra competenza come psicologi, identifichiamo i profili più in linea con la visione aziendale.";
+});
+
+document.getElementById("card-secondo").addEventListener("click",function(){
+    document.getElementById("titoloCard").innerHTML="Pharma e Healthcare";
+    document.getElementById("testoCard").innerHTML="Supportiamo le aziende in ambito farmaceutico e sanitario, nella ricerca di profili tecnici e commerciali.";
+});
+
+document.getElementById("card-terzo").addEventListener("click",function(){
+    document.getElementById("titoloCard").innerHTML="Financial Service";
+    document.getElementById("testoCard").innerHTML="Siamo partener di istituti bancari e finanziari per la selezione di figure specializzate.";
+});
